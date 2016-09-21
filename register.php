4e9ba107e97185da602246a59a5bdd77df067ba5
Giorgio Sartore <mani@Mac-mini-di-Giorgio.local>
2016-09-21 21:03:49 +0200

    register

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://nagabotel.herokuapp.com/execute.php';
+$BOT_TOKEN = '251140608:AAHavKfqor2GyqYMf9p_DN6IJQw1KBVut3Q';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
